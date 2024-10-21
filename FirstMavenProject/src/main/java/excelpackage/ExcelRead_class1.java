eb2fee1058e98096b017e19340da2f0a1d91eabc
VAISHNAVI <VAISHNAVI@DESKTOP-00BRNAR>
2024-10-21 20:21:00 +0530

    ExcelRead sample commit

---
+++ FirstMavenProject/src/main/java/excelpackage/ExcelRead_class1.java
@@ -0,0 +1,43 @@
+package excelpackage;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelRead_class1 {
+public static FileInputStream f;
+public static XSSFWorkbook w;
+public static XSSFSheet s;
+
+public static String getStringdata(int a,int b) throws IOException
+{
+	f=new FileInputStream("C:\\Users\\VAISHNAVI\\Downloads\\Salaryexcel.xlsx");
+	w=new XSSFWorkbook(f);
+	s=w.getSheet("Sheet1");
+	XSSFRow r=s.getRow(a);
+	XSSFCell c=r.getCell(b);
+return c.getStringCellValue();
+}
+	public static String getIntegerdata(int a,int b) throws IOException
+	{
+		f=new FileInputStream("C:\\Users\\VAISHNAVI\\Downloads\\Salaryexcel.xlsx");
+		w=new XSSFWorkbook(f);
+		s=w.getSheet("Sheet1");
+		XSSFRow r=s.getRow(a);
+		XSSFCell c=r.getCell(b);
+		int x=(int)c.getNumericCellValue();
+		return String.valueOf(x);
+}
+}
+
+
+
+
+
+
+
---
+++ FirstMavenProject/src/main/java/excelpackage/ExcelRead_class2.java
@@ -0,0 +1,14 @@
+package excelpackage;
+
+import java.io.IOException;
+
+public class ExcelRead_class2 {
+
+	public static void main(String[] args) throws IOException {
+	System.out.println(ExcelRead_class1.getStringdata(1, 0));
+	System.out.println(ExcelRead_class1.getIntegerdata(1, 1));
+	System.out.println(ExcelRead_class1.getStringdata(2, 0));
+	System.out.println(ExcelRead_class1.getIntegerdata(2, 1));
+	}
+
+}
